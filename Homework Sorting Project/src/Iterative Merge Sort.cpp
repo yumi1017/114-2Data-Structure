5bf4ddcd2adde9422d99502c9b2b2c343e475337
yumi1017 <136673423+yumi1017@users.noreply.github.com>
2026-06-24 00:24:03 +0800

    Rename Homework Sorting Project/Iterative Merge Sort.cpp to Homework Sorting Project/src/Iterative Merge Sort.cpp

--- Homework Sorting Project/src/Iterative Merge Sort.cpp
+++ Homework Sorting Project/src/Iterative Merge Sort.cpp
@@ -71,12 +71,12 @@ int main()
 {
     int n;
 
-    cout << "�п�J��Ƶ��� n: ";
+    cout << "½Ð¿é¤J¸ê®Æµ§¼Æ n: ";
     cin >> n;
 
     int* a = new int[n + 1];
 
-    cout << "�п�J " << n << " �ӼƦr: ";
+    cout << "½Ð¿é¤J " << n << " ­Ó¼Æ¦r: ";
     for (int i = 1; i <= n; i++)
     {
         cin >> a[i];
@@ -84,7 +84,7 @@ int main()
 
     MergeSort(a, n);
 
-    cout << "�Ƨǫᵲ�G: ";
+    cout << "±Æ§Ç«áµ²ªG: ";
     for (int i = 1; i <= n; i++)
     {
         cout << a[i] << " ";
